8891d9aa21cdcc50964c2275af1fb39bbfb7cb0e
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-07 17:15:04 +0530

    add user models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+"Module for models"
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    "Model for user profile"
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar_url = models.URLField(blank=True, null=True)
+    uid = models.CharField(max_length=255, blank=True, null=True)
+
+class UserRepositories(models.Model):
+    "Model for user repositories"
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='repositories')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    html_url = models.URLField(blank=True, null=True)
+
