c3e70807630a151bbc1435e7a0cc63ddf718a94b
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-07 17:14:39 +0530

    register user models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserProfile, UserRepositories
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserRepositories)
