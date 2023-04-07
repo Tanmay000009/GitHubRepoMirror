fb6583d4e1083bce062eb33db15ee9c387c6c4f0
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-07 23:23:55 +0530

    store user repositories

--- core/models.py
+++ core/models.py
@@ -13,6 +13,11 @@ class UserRepositories(models.Model):
     "Model for user repositories"
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='repositories')
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    html_url = models.URLField(blank=True, null=True)
+    id = models.IntegerField(primary_key=True)
+    owner_id = models.IntegerField(blank=True, null=True)
+    owner_name = models.CharField(max_length=255, blank=True, null=True)
+    owner_email = models.EmailField(blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    stars = models.IntegerField(blank=True, null=True)
+
 
--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.core.exceptions import ObjectDoesNotExist
+from .models import UserRepositories
 import requests
+import json
 # Create your views here.
 
 
@@ -21,22 +24,87 @@ def load_repositorios(request):
 def get_repositorios(request):
     id = request.user.id
     access_token = request.user.userprofile.access_token
+    exisitingRepos = UserRepositories.objects.filter(user=id)
 
-    url = 'https://api.github.com/user/repos'
+    if exisitingRepos:
+        return JsonResponse({"status": "success"})
 
     headers = {
         'Authorization': f'token {access_token}',
         'Accept': 'application/vnd.github.v3+json'
     }
 
-    response = requests.get('https://api.github.com/user/repos', headers=headers)
+    page_num = 1
+    repos = []
+    failure = False
+    unauthorized = False
 
-    if response.status_code == 200:
-        data = {"status": "success"}
-    elif response.status_code == 401:
-        logout(request)
-        data = {"status": "unauthorized"}
-    else:
-        data = {"status": "error"}
+    while True:
+        response = requests.get(f'https://api.github.com/user/repos?per_page=30&page={page_num}', headers=headers)
 
-    return JsonResponse(data)
+        if response.status_code == 401:
+            logout(request)
+            unauthorized = True
+            break
+
+        if response.status_code != 200:
+            response = requests.get(f'https://api.github.com/user/repos?per_page=30&page={page_num}', headers=headers)
+            
+            if response.status_code == 401:
+                logout(request)
+                unauthorized = True
+                break
+            
+            if response.status_code != 200:
+                failure = True
+                break
+
+
+        repos_page = json.loads(response.text)
+        repos.extend(repos_page)
+
+        if len(repos_page) == 0:
+            break
+
+        page_num += 1
+
+    if unauthorized:
+        return JsonResponse({"status": "unauthorized"})
+    
+    if failure:
+        return JsonResponse({"status": "failure"})
+
+    filteredData = [{
+        "id": repo["id"],
+        "name": repo["name"],
+        "owner_id": repo["owner"]["id"],
+        "owner_name": repo["owner"]["login"],
+        "owner_email": repo["owner"]["email"] if "email" in repo["owner"] else None,   
+        "status": "private" if repo["private"] else "public",
+        "stars": repo["stargazers_count"],
+    } for repo in repos]
+        
+    # save or update repositories
+    for repo in filteredData:
+        try:
+            user_repo = UserRepositories.objects.get(id=repo['id'])
+            user_repo.name = repo['name']
+            user_repo.owner_id = repo['owner_id']
+            user_repo.owner_name = repo['owner_name']
+            user_repo.owner_email = repo['owner_email']
+            user_repo.status = repo['status']
+            user_repo.stars = repo['stars']
+            user_repo.save()
+        except ObjectDoesNotExist:
+            user_repo = UserRepositories.objects.create(
+                user=request.user,  
+                name=repo['name'],
+                id=repo['id'],
+                owner_id=repo['owner_id'],
+                owner_name=repo['owner_name'],
+                owner_email=repo['owner_email'],
+                status=repo['status'],
+                stars=repo['stars']
+            )
+
+    return JsonResponse({"status": "success"})
