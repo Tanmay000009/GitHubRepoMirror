db3346fe21bf5e4b96343d2ea1b4eaddd57873ab
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-08 16:27:48 +0530

    register organizations model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, UserRepositories
+from .models import UserProfile, UserRepositories, UserOrganizations
 
 admin.site.register(UserProfile)
 admin.site.register(UserRepositories)
+admin.site.register(UserOrganizations)
