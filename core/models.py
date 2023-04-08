fe2e79f7234cd50aed3b9ffbbaaaeacd552d3b7b
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-08 16:27:42 +0530

    edit Repositories model

--- core/models.py
+++ core/models.py
@@ -8,10 +8,21 @@ class UserProfile(models.Model):
     avatar_url = models.URLField(blank=True, null=True)
     uid = models.CharField(max_length=255, blank=True, null=True)
     access_token = models.CharField(max_length=255, blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+
+class UserOrganizations(models.Model):
+    "Model for user organizations"
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizations')
+    name = models.CharField(max_length=255)
+    id = models.IntegerField(primary_key=True)
+    url = models.URLField(blank=True, null=True)
+    avatar_url = models.URLField(blank=True, null=True)
+    
 
 class UserRepositories(models.Model):
     "Model for user repositories"
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='repositories')
+    organization = models.ManyToOneRel(UserOrganizations, related_name='repositories', to='UserOrganizations', on_delete=models.CASCADE, field_name='organization')
     name = models.CharField(max_length=255)
     id = models.IntegerField(primary_key=True)
     owner_id = models.IntegerField(blank=True, null=True)
@@ -19,13 +30,4 @@ class UserRepositories(models.Model):
     owner_email = models.EmailField(blank=True, null=True)
     status = models.CharField(max_length=255, blank=True, null=True)
     stars = models.IntegerField(blank=True, null=True)
-
-class UserOrganizations(models.Model):
-    "Model for user organizations"
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizations')
-    name = models.CharField(max_length=255)
-    id = models.IntegerField(primary_key=True)
-    owner_id = models.IntegerField(blank=True, null=True)
-    owner_name = models.CharField(max_length=255, blank=True, null=True)
-    owner_email = models.EmailField(blank=True, null=True)
-    
+    url = models.URLField(blank=True, null=True)
