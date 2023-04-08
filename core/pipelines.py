355169f7032e5093fcc990d45bda284b8689f714
tanmay000009 <tanmayvyas009@gmail.com>
2023-04-08 14:01:43 +0530

    fix authentication flow | store new access token

--- core/pipelines.py
+++ core/pipelines.py
@@ -1,11 +1,11 @@
 "Pipelines for social auth"
-from core.models import UserProfile, UserRepositories
+from core.models import UserProfile
 
 from .models import User
 
 def create_user_profile(backend, user, response, *args, **kwargs):
     "Create user profile with avatar url and uid"
-    profile = User.objects.filter(username=user).first()
+    profile = UserProfile.objects.filter(user=user).first()
     if not profile:
         profile = UserProfile(user=user)
     if 'avatar_url' in response:
